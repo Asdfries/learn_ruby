40bd7bb75c6dd3f9490036ecf066e40276dd541b
asdfries <aaronfriesen1986@gmail.com>
2016-12-11 17:26:21 -0600

    added code to make non optional tests pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(first, second)
+	first + second
+end
+
+def subtract(first, second)
+	first - second
+end
+
+def sum(given)
+	x = 0 
+	for i in given do 
+		x += i
+	end
+	return x
+end
+
+def multiply(first, second)
+	first * second
+end
